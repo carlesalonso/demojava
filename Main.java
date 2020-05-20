26e1981d80e1d7d6ccc52837fb32e653b7ed493b
Carles Alonso Martinez <carlos.martinez@mataro.epiaedu.cat>
2020-05-20 11:04:52 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,7 +16,7 @@ public class Main {
             for(j=0; j<=i; j++) 
             { 
                 // printing stars 
-                System.out.print("* "); 
+                System.out.print("O"); 
             } 
   
             // ending line after each row 
